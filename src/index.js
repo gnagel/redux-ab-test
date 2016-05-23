dbf17e993ccc6f5ff1f6b25f752c1ceeee625641
Glenn Nagel <glenn@mercury-wireless.com>
2016-05-22 22:09:45 -0400

    Working on es5 compliant build package

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Debugger from './debugger';
+export Debugger;
+
+import Experiment from './experiment';
+export Experiment;
+
+import Variation from './variation';
+export Variation;
+
+import reactAbTest, { VariationType, ExperimentType, constants, actions, initialState, reducers, selectors } from './module';
+export default reactAbTest;
+export VariationType;
+export ExperimentType;
+export constants;
+export actions;
+export initialState;
+export reducers;
+export selectors;
