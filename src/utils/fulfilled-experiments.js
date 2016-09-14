8a49c1b05a67f9f2ae15fda136683bcc1eb01665
Glenn Nagel <glenn@mercury-wireless.com>
2016-09-13 20:42:51 -0400

    Fixed extra )

--- src/utils/fulfilled-experiments.js
+++ src/utils/fulfilled-experiments.js
@@ -5,7 +5,7 @@ import getKey from './get-key';
  * Find all experiments that have been fulfilled and shouldn't be run again.
  */
 const fulfilledExperiments = ({experiments, active, winners, key_path, single_success_path}) => {
-  experiments = experiments.filter( experiment => experiment.getIn(single_success_path, false)) );
+  experiments = experiments.filter( experiment => experiment.getIn(single_success_path, false) );
   return experiments.map(getKey).filter(active.has).filter(winners.has);
 };
 export default fulfilledExperiments;
