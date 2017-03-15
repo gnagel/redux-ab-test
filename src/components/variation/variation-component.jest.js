f2e867c126176e39665e84a094d2708625fb191d
Glenn Nagel <glenn@mercury-wireless.com>
2017-03-15 13:35:12 -0400

    Updated snapshots and tests

--- src/components/variation/variation-component.jest.js
+++ src/components/variation/variation-component.jest.js
@@ -51,50 +51,39 @@ describe('(Component) src/components/variation/variation.js', () => {
     expect(tree.props['data-experiment-name']).toEqual('Test-experimentName');
     expect(tree.props['data-variation-id']).toEqual('test-id-variation-b');
     expect(tree.props['data-variation-name']).toEqual('Variation B');
-    expect(tree.children).toEqual(['Test-children']);
   });
 
   it('has the correct text', () => {
-    expect(component).to.have.text('Test-children');
+    expect(tree).toMatchSnapshot();
+    expect(tree.children).toEqual(['Test-children']);
   });
 
   it('has the correct tagName', () => {
-    expect(shallowComponent).to.have.tagName('span');
-  });
-
-  it('wraps the text in a span', () => {
-    const child = component.find('span');
-    expect(child).to.have.tagName('span');
-    expect(child).to.have.text('Test-children');
+    expect(tree).toMatchSnapshot();
+    expect(tree.type).toEqual('span');
   });
 
   it('have the correct data-* props', () => {
-    const child = shallowComponent.find('span');
-    expect(child).to.have.prop('data-variation-id', 'test-id-variation-b');
-    expect(child).to.have.prop('data-variation-name', 'Variation B');
-    expect(child).to.have.prop('data-experiment-id', 'test-experimentId');
-    expect(child).to.have.prop('data-experiment-name', 'Test-experimentName');
-  });
-
-  it('has the correct data-* attrs', () => {
-    const child = shallowComponent.find('span');
-    expect(child).to.have.data('variation-id', 'test-id-variation-b');
-    expect(child).to.have.data('variation-name', 'Variation B');
-    expect(child).to.have.data('experiment-id', 'test-experimentId');
-    expect(child).to.have.data('experiment-name', 'Test-experimentName');
+    expect(tree).toMatchSnapshot();
+    expect(tree.props['data-variation-id']).toEqual('test-id-variation-b');
+    expect(tree.props['data-variation-name']).toEqual('Variation B');
+    expect(tree.props['data-experiment-id']).toEqual('test-experimentId');
+    expect(tree.props['data-experiment-name']).toEqual('Test-experimentName');
   });
 
   it('adds the props to the children component', () => {
     props = { ...props, children: (<div id="test-id">Test-children</div>) };
-    component = shallow(
+    component = renderer.create(
       <Variation {...props} />
     );
-    expect(component).to.have.tagName('div');
-    expect(component).to.have.attr('id', 'test-id');
-    expect(component).to.have.data('variation-id', 'test-id-variation-b');
-    expect(component).to.have.data('variation-name', 'Variation B');
-    expect(component).to.have.data('experiment-id', 'test-experimentId');
-    expect(component).to.have.data('experiment-name', 'Test-experimentName');
-    expect(component).to.have.text('Test-children');
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(tree.type).toEqual('div');
+    expect(tree.props['id']).toEqual('test-id');
+    expect(tree.props['data-variation-id']).toEqual('test-id-variation-b');
+    expect(tree.props['data-variation-name']).toEqual('Variation B');
+    expect(tree.props['data-experiment-id']).toEqual('test-experimentId');
+    expect(tree.props['data-experiment-name']).toEqual('Test-experimentName');
+    expect(tree.children).toEqual(['Test-children']);
   });
 });
